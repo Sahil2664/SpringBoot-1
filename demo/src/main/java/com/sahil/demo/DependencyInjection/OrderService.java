875a3783cd95b322f07e07244abc935ea6297c2e
Sahil Kumar Singh <152484304+Sahil2664@users.noreply.github.com>
2026-07-13 14:32:25 +0530

    Dependency Injection

---
+++ demo/src/main/java/com/sahil/demo/DependencyInjection/OrderService.java
@@ -0,0 +1,25 @@
+package com.sahil.demo.DependencyInjection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+    // Field Injection
+ //   @Autowired
+    PaymentService paymentService;
+    // Constructor Injection
+   // @Autowired
+    OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+    // Setter Injection
+  //  @Autowired
+    public void setPaymentService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+    public void placeOrder() {
+        paymentService.payment();
+        System.out.println("Order placed");
+    }
+}
