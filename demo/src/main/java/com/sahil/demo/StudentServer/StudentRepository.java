4c445c6c8ad3e3404eff38d72b5119a30b77470b
Sahil Kumar Singh <152484304+Sahil2664@users.noreply.github.com>
2026-07-15 11:00:50 +0530

    StudentServer

--- demo/src/main/java/com/sahil/demo/StudentServer/StudentRepository.java
+++ demo/src/main/java/com/sahil/demo/StudentServer/StudentRepository.java
@@ -1,2 +1,12 @@
-package com.sahil.demo.StudentServer;public class StudentRepository {
+package com.sahil.demo.StudentServer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository extends JpaRepository<Student, Integer>{
+
+ //   public Student save(Student student) {
+  //      System.out.println("Student Information saved");
+  //      return student;
+    }
 }
--- demo/src/main/java/com/sahil/demo/StudentServer/StudentServer.java
+++ demo/src/main/java/com/sahil/demo/StudentServer/StudentServer.java
@@ -1,2 +1,11 @@
-package com.sahil.demo.StudentServer;public class StudentServer {
+package com.sahil.demo.StudentServer;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentServer {
+    // 1. Store the Student information into the database
+
+
+    // 2. get the stored student information
 }
